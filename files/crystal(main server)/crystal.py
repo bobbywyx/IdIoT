2153b492e7b609ff4c6d3730079d74fac0564213
bobbywyx <yixinbobbywu@outlook.com>
2021-03-06 14:06:20 +0800

    constructing

--- files/crystal(main server)/crystal.py
+++ files/crystal(main server)/crystal.py
@@ -2,13 +2,26 @@
 import time
 import json
 
-
 from EventManager import EventManager
-
+# import EventManager
 
 def b(a=1):
     for i in range(a):
         print('ok')
+
+
+class Crystal:
+    def __init__(self,hostname,version,id_level,):
+        self.hostname = hostname
+        self.version = version
+        self.id_level = id_level
+
+    def pop_information(self):
+        information = 'IdIoT Server Information'
+        information+= 'version:\n   IdIoT:'+str(self.version)
+        # print('version:\n   IdIoT:',self.version)
+        # print('hostname:\n  name:',self.hostname)
+        print(information)
 
 
 if __name__ == '__main__':
@@ -17,4 +30,8 @@ if __name__ == '__main__':
     EventMgr = EventManager()
 
     EventMgr.register_event_listener('a', b)
-    EventMgr.broadcast_event('a', None, False)
+    # EventMgr.broadcast_event('a', None, False)
+
+    server = Crystal('IdIoT-starter', '0.0.1-py', 15)
+
+    server.pop_information()
